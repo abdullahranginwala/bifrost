6dee76da0385b6a1b853804ae7a91fc4af44b81e
Abdullah Ranginwala <abdullahranginwala20@gmail.com>
2023-09-14 22:24:38 +0530

    added chat and user routes

---
+++ src/routes/chatRoutes.ts
@@ -0,0 +1,29 @@
+import { Chat } from '../models/Chat';
+import { authenticate } from '../middleware/auth';
+
+app.post('/send', authenticate, async (req, res) => {
+    const { recipientId, content } = req.body;
+    let chat = await Chat.findOne({ participants: { $all: [req.userId, recipientId] } });
+
+    if (!chat) {
+        chat = new Chat({ participants: [req.userId, recipientId], messages: [] });
+    }
+
+    chat.messages.push({
+        sender: req.userId,
+        content,
+        timestamp: new Date(),
+    });
+
+    await chat.save();
+    res.status(201).send('Message sent');
+});
+
+app.post('/create-group', authenticate, async (req, res) => {
+    const { participants } = req.body;
+
+    const chat = new Chat({ participants, messages: [] });
+    await chat.save();
+
+    res.status(201).send('Group created');
+});
---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,30 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { User } from '../models/User';
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+        username,
+        password: hashedPassword,
+    });
+
+    await user.save();
+    res.status(201).send('User registered');
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+    if (!user) return res.status(400).send('Invalid credentials');
+
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) return res.status(400).send('Invalid credentials');
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ token });
+});
