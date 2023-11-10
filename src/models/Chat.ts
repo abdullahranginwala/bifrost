c49cf662c9f1447b1dadaa12c3447ee2896c42cc
Abdullah Ranginwala <abdullahranginwala20@gmail.com>
2023-11-10 13:17:29 +0530

    added chat name in schema

--- src/models/Chat.ts
+++ src/models/Chat.ts
@@ -3,11 +3,13 @@ import { IUser } from "./User";
 import { IMessage } from "./Message";
 
 export interface IChat extends Document {
+    name: String,
     participants: IUser['_id'][];
     messages: IMessage[];
 }
 
 const chatSchema = new Schema<IChat>({
+    name: String,
     participants: [{type:Schema.Types.ObjectId, ref: 'User'}],
     messages: [{type:Schema.Types.ObjectId, ref: 'Message'}],
 });
